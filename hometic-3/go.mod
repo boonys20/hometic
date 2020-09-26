1247754869fb67937833df12e1f6b9da594e368e
Kung Kub <kungcs20@gmail.com>
2020-09-26 09:34:42 +0700

    update to latest

--- hometic-3/go.mod
+++
@@ -1,8 +0,0 @@
-module github.com/boonys20/hometic
-
-go 1.15
-
-require (
-	github.com/gorilla/mux v1.8.0
-	github.com/lib/pq v1.8.0
-)
--- hometic/server.go
+++ hometic/server.go
@@ -17,50 +17,55 @@ type Pair struct {
 	UserID   int64
 }
 
-type PairDeviceHandler struct {
-	createPairDevice CreatePairDevice
+type Device interface {
+	Pair(p Pair) error
 }
 
-type CreatePairDevice func(p Pair) error
+type CreatePairDeviceFunc func(p Pair) error
 
-func (ph *PairDeviceHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	var p Pair
-	err := json.NewDecoder(r.Body).Decode(&p)
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		json.NewEncoder(w).Encode(err.Error())
-		return
-	}
-	defer r.Body.Close()
-	fmt.Printf("pair: %#v\n", p)
-
-	err = ph.createPairDevice(p)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		json.NewEncoder(w).Encode(err.Error())
-		return
-	}
-
-	w.Write([]byte(`{"status":"active"}`))
+func (fn CreatePairDeviceFunc) Pair(p Pair) error {
+	return fn(p)
 }
 
-func createPairDevice(p Pair) error {
-	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
-	if err != nil {
-		log.Fatal("connect to database error", err)
+func NewCreatePairDevice(db *sql.DB) CreatePairDeviceFunc {
+	return func(p Pair) error {
+		_, err := db.Exec("INSERT INTO pairs VALUES ($1,$2);", p.DeviceID, p.UserID)
+		return err
 	}
+}
 
-	_, err = db.Exec("INSERT INTO pairs (DEVICE_ID, USER_ID) values ($1, $2) RETURNING id", p.DeviceID, p.UserID)
+func PairDeviceHandler(device Device) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var p Pair
+		err := json.NewDecoder(r.Body).Decode(&p)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			json.NewEncoder(w).Encode(err.Error())
+			return
+		}
+		defer r.Body.Close()
+		fmt.Printf("pair: %#v\n", p)
 
-	return err
-
+		err = device.Pair(p)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			json.NewEncoder(w).Encode(err.Error())
+			return
+		}
+		w.Write([]byte(`{"status":"active"}`))
+	}
 }
 
 func main() {
 	fmt.Println("hello hometic : I'm Gopher!!")
 
+	db, err := sql.Open("postgres", os.Getenv("postgres://qrulmoqq:3Zu_Xhw121TaveedaBFPEQ_5Z_MGVel6@john.db.elephantsql.com:5432/qrulmoqq"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	r := mux.NewRouter()
-	r.Handle("/pair-device", &PairDeviceHandler{createPairDevice}).Methods(http.MethodPost)
+	r.Handle("/pair-device", PairDeviceHandler(NewCreatePairDevice(db))).Methods(http.MethodPost)
 
 	addr := fmt.Sprintf("0.0.0.0:%s", os.Getenv("PORT"))
 	fmt.Println("addr:", addr)
